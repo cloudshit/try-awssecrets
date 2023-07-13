d1e2bc3a7e697657370b169ada73089922c9a11c
pmh-only <opensource@pmh.codes>
2023-07-13 19:18:48 +0900

    add driver

--- src/main.go
+++ src/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type DBSecret struct {
